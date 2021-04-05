33bb0ca240e432f6179852e683e96787463b799d
XyZerKunG <64718093+XyZerKunG@users.noreply.github.com>
2021-04-05 22:47:46 +0700

    new message handler

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const {app, BrowserWindow, Menu, ipcMain, remote} = require('electron')
+const {app, BrowserWindow, Menu, ipcMain, ipcRenderer} = require('electron')
+const ipc = ipcRenderer
 const { link } = require('fs')
-const ipc = ipcMain
 const path = require('path')
 const url = require('url')
 
 // Windows
 let index
+
+let type = "unknown"
 
 function createwindows() {
     index = new BrowserWindow({
@@ -19,26 +21,63 @@ function createwindows() {
         'minWidth': 800,
         'minHeight': 480
     })
-    //index.openDevTools()
     Menu.setApplicationMenu(new Menu())
     index.loadURL(url.format({
         pathname: path.join(__dirname, "index.html"),
         protocol: 'file:',
         slashes: true
     }))
-    ipc.on('*e^Q$xV?z>6[$X@9', () => {
-        index.minimize()
-    })
-    ipc.on('A%Q3,BUNbw6Sxjtw', () => {
-        if (!index.isMaximized()) {
-            index.maximize()
-        }else {
-            index.unmaximize()
+    ipcMain.on('async-message', (event, arg) => {
+        console.log(arg)
+        switch (arg) {
+            case "*e^Q$xV?z>6[$X@9":
+                index.minimize()
+                break
+            case "A%Q3,BUNbw6Sxjtw":
+                if (!index.isMaximized()) {
+                    index.maximize()
+                }else {
+                    index.unmaximize()
+                }
+                break
+            case "X=E[8}N&L;j6nN}9":
+                index.close()
+                break
+            case "snapshot":
+                type = 'snapshot'
+                pageSetup()
+                break
+            case "bukkit":
+                type = 'bukkit'
+                pageSetup()
+                break
+            case "paper":
+                type = 'paper'
+                pageSetup()
+                break
+            case "origin":
+                type = 'origin'
+                pageSetup()
+                break
+            case "Ready! ? setup":
+                ipc.sendSync("async-message", "id0:" + type)
+                break
+            case "index":
+                pageSetup()
+                break
+            default:
+                console.log("Unknown Message: " + arg)
+                break
         }
     })
-    ipc.on('X=E[8}N&L;j6nN}9', () => {
-        index.close()
-    })
+}
+
+function pageSetup() {
+    index.loadURL(url.format({
+        pathname: path.join(__dirname, "setup.html"),
+        protocol: 'file:',
+        slashes: true
+    }))
 }
 
 app.on('ready', createwindows)
