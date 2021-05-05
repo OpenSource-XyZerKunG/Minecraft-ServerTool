6867091afe22d7e44dd6bcea782c5bc81d67d298
XyZerKunG <64718093+XyZerKunG@users.noreply.github.com>
2021-05-05 23:03:25 +0700

    add function
    - add function windows and mac display terminal and folder

--- build/js/setup.js
+++ build/js/setup.js
@@ -8,12 +8,38 @@ window.addEventListener("DOMContentLoaded", () => {
     socket.emit("post:app", "get:type")
     socket.on("post:type", (message) => {
         document.getElementById("type").innerText = "Type: " + String(message)
-    }) 
+    })
+    if (process.platform == "win32") {
+        const link = document.createElement("link")
+        link.rel = "stylesheet"
+        link.href = "css/terminal-windows.css"
+        document.getElementById("folderimage").src = "img/win-file.png"
+        document.body.appendChild(link)
+    }else if (process.platform == "darwin") {
+        const link = document.createElement("link")
+        link.rel = "stylesheet"
+        link.href = "css/terminal-mac.css"
+        document.body.appendChild(link)
+        document.getElementById("folderimage").src = "img/mac-file.png"
+    }
 })
 
 const consoletitle = document.getElementById("consoletitle")
+const foldername = document.getElementById("foldername")
 const terminallabel = document.getElementById("terminal-text")
+const folderlabel = document.getElementById("folderlabel")
 
 consoletitle.addEventListener("input", () => {
     terminallabel.innerText = consoletitle.value
+})
+
+foldername.addEventListener("input", () => {
+    folderlabel.innerText = foldername.value
+})
+
+document.getElementById("done").addEventListener("click", () => {
+    if (String(consoletitle.value).replaceAll(" ", "") != "" && String(foldername.value).replaceAll(" ", "") != "") {
+        socket.emit("post:data", consoletitle.value + "," + foldername.value)
+        window.location.href = "final.html"
+    }
 })
