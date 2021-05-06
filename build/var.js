886ea40d410c6a68c492ecaf7b4e8bac5db06c40
XyZerKunG <64718093+XyZerKunG@users.noreply.github.com>
2021-05-07 00:01:47 +0700

    add envvar and autorun

--- build/var.js
+++ build/var.js
@@ -5,10 +5,12 @@ var SPIGOTMC = "SpigotMC";
 var PAPERMC = "PaperMC";
 var title = "";
 var folder = "";
+var envvar = "";
 var version = "";
 var type = "";
 var nogui = false;
 var eula = false;
+var autorun = false;
 module.exports = {
     RELEASE: RELEASE, SNAPSHOT: SNAPSHOT, SPIGOTMC: SPIGOTMC, PAPERMC: PAPERMC, title: title, folder: folder, version: version, type: type, nogui: nogui, eula: eula
 };
--- src/var.ts
+++ src/var.ts
@@ -5,10 +5,12 @@ let PAPERMC:string = "PaperMC"
 
 let title:string = ""
 let folder:string = ""
+let envvar:string = ""
 let version:string = ""
 let type:string = ""
 let nogui:boolean = false
 let eula:boolean = false
+let autorun:boolean = false
 
 module.exports = {
     RELEASE, SNAPSHOT, SPIGOTMC, PAPERMC, title, folder, version, type, nogui, eula
