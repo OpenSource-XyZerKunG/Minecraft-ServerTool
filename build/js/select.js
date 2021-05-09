eca4122d6f3c2aa3af23e3014fe3e0c171e6a232
XyZerKunG <64718093+XyZerKunG@users.noreply.github.com>
2021-05-09 22:11:32 +0700

    Add function send type of minecraft server to localserver

--- build/js/select.js
+++ build/js/select.js
@@ -2,6 +2,8 @@ const origin = document.getElementById("origin")
 const snapshot = document.getElementById("snapshot")
 const bukkit = document.getElementById("bukkit")
 const paper = document.getElementById("paper")
+const purpur = document.getElementById("purpur")
+const yatopia = document.getElementById("yatopia")
 const types = require("./var")
 
 origin.addEventListener("click", () => {
@@ -21,6 +23,26 @@ bukkit.addEventListener("click", () => {
 
 paper.addEventListener("click", () => {
     socket.emit("post:type", types.PAPERMC)
+    setup()
+})
+
+bukkit.addEventListener("click", () => {
+    socket.emit("post:type", types.SPIGOTMC)
+    setup()
+})
+
+paper.addEventListener("click", () => {
+    socket.emit("post:type", types.PAPERMC)
+    setup()
+})
+
+purpur.addEventListener("click", () => {
+    socket.emit("post:type", types.PURPURMC)
+    setup()
+})
+
+yatopia.addEventListener("click", () => {
+    socket.emit("post:type", types.YATOPIA)
     setup()
 })
 
