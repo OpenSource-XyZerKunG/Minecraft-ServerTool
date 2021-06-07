b6c0e518cb6231a880c7cd12ffd43c674f38b62e
XyZerKunG <64718093+XyZerKunG@users.noreply.github.com>
2021-06-07 21:27:02 +0700

    add function execute terminalvar

--- build/js/terminal.js
+++ build/js/terminal.js
@@ -6,6 +6,7 @@ const fitAddon = new FitAddon()
 const unicodeAddon = new Unicode11Addon()
 const term = new Terminal()
 const terminal = document.getElementById("terminal")
+const terminalname = document.getElementById("terminalname")
 
 term.loadAddon(unicodeAddon)
 term.loadAddon(new WebLinksAddon())
@@ -13,7 +14,6 @@ term.loadAddon(fitAddon)
 fitAddon.fit()
 term.unicode.activeVersion = "11"
 term.open(terminal)
-ipcRenderer.send("terminal.keystroke", "\r")
 
 ipcRenderer.on("terminal.incomingData", (event, data) => {
     term.write(data)
@@ -21,4 +21,27 @@ ipcRenderer.on("terminal.incomingData", (event, data) => {
 
 term.onData(e => {
     ipcRenderer.send("terminal.keystroke", e)
+})
+
+ipcRenderer.on("post:terminalname", (event, data) => {
+    if (String(data.name).replaceAll(" ", "") != "") {
+        terminalname.innerText = data.name
+        document.getElementById("ngrokhidebox").style.opacity = 1
+        const scriptngrok = document.createElement("script")
+        scriptngrok.src = "js/ngrok.js"
+        document.body.appendChild(scriptngrok)
+    }
+    let dataexecute = String(data.execute)
+    if (dataexecute.replaceAll(" ", "") != "") {
+        for (intexecute in dataexecute) {
+            ipcRenderer.send("terminal.keystroke", dataexecute[intexecute])
+        }
+        ipcRenderer.send("terminal.keystroke", "\r")
+    } else {
+        ipcRenderer.send("terminal.keystroke", "\r")
+    }
+})
+
+window.addEventListener("DOMContentLoaded", () => {
+    ipcRenderer.send("post:app", "get:terminalname")
 })
