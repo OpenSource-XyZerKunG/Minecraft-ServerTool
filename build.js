e198371a9e8c0cfc68b44a9e1ee6046f8632a663
XyZerKunG <64718093+XyZerKunG@users.noreply.github.com>
2021-08-30 15:11:42 +0700

    update build script

---
+++ build.js
@@ -0,0 +1,29 @@
+if (process.argv.length != 3) throw new Error("Syntax: node build.js <outputdir>")
+
+const fs = require("fs")
+const path = require("path")
+
+async function deletes(pathurl) {
+    try {
+        if (fs.statSync(pathurl).isDirectory()) {
+            const files = await fs.readdirSync(pathurl)
+            files.forEach(async (value, index) => {
+                deletes(path.join(pathurl, value))
+            })
+            await fs.rmdirSync(pathurl)
+        } else {
+            await fs.unlinkSync(pathurl)
+        }
+    } catch (ignore) {}
+}
+
+fs.readdir(path.join(__dirname, process.argv[2]), {}, (ignore, files) => {
+    files.forEach(async (value, index) => {
+        await deletes(path.join(__dirname, process.argv[2], value, "resources", "app", "README.md"))
+        await deletes(path.join(__dirname, process.argv[2], value, "resources", "app", ".gitignore"))
+        await deletes(path.join(__dirname, process.argv[2], value, "resources", "app", "src"))
+        await deletes(path.join(__dirname, process.argv[2], value, "resources", "app", "build.js"))
+        await deletes(path.join(__dirname, process.argv[2], value, "resources", "app", "tsconfig.json"))
+        console.log(`Done ${value}!`)
+    })
+})
